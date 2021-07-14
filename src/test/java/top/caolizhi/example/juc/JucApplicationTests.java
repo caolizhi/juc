9898f5dd10b28ad8e323f96d15c10e9854366c68
cao-lizhi <cao.lizhi@navercorp.com>
2021-07-14 15:30:42 +0800

    first init

---
+++ src/test/java/top/caolizhi/example/juc/JucApplicationTests.java
@@ -0,0 +1,13 @@
+package top.caolizhi.example.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
